0e5c9a381c13c3000b1032d5d615ed12fb6bd0b3
Dr. Markus Voss <voss.markus@gmx.net>
2021-05-31 15:01:11 +0200

    Update of example pictures for Examples.md

--- pumla.py
+++ pumla.py
@@ -6,7 +6,7 @@ element re-usability.
 __author__ = "Dr. Markus Voss (private person)"
 __copyright__ = "(C) Copyright 2021 by Dr. Markus Voss (private person)"
 __license__ = "GPL"
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __maintainer__ = "Dr. Markus Voss (private person)"
 __status__ = "Development"
 
