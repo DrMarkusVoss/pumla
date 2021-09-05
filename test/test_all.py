74251d959ffa5aee7e5c61be08275a13aa1a3dd7
Jan Bernloehr <jan@bernloehrs.de>
2021-09-05 09:53:10 +0200

    Turn into python package

--- test/test_all.py
+++ test/test_all.py
@@ -1,7 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../")
-
 from test_cases.test_pumla_cmd_utils import getTestClass as getCmdUtilsTestClass
 
 def testAll():
