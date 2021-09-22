bf0c74cb77f3a3299ca0eae29e631d599d877ae3
Dr. Markus Voss <voss.markus@gmx.net>
2021-09-22 09:11:26 +0200

    fix for latest commit (?)

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 """setup procedure"""
-import os
-
 from setuptools import setup, find_packages
+import os
 
 setup(
     packages=find_packages(where="src"),
@@ -9,7 +8,19 @@ setup(
     extras_require={"dev": ["mypy==0.910", "pylint==2.10.2"]},
 )
 
-print("pumla setup: initialising the examples")
-os.chdir("./test/examples")
-os.system("pumla init")
-print("pumla setup done.")
+curpath = os.path.abspath(os.path.curdir)
+print("Path to initialise: " + curpath)
+print("Path of pumla installation: " + curpath)
+mainpath = curpath + "/"
+pumla_macros_fn = curpath + "/test/examples/pumla_macros.puml"
+pm_comment = "' THIS IS AN AUTOMATICALLY GENERATED FILE BY pumla init \n"
+pm_comment = pm_comment + "' DO NOT CHANGE MANUALLY!\n"
+pm_comment = pm_comment + "' TO ADOPT THE PATHS TO YOUR SYSTEM, CALL pumla init AGAIN\n"
+pm_comment = pm_comment + "' IN THE FOLDER OF THIS FILE HERE!\n"
+pm_include_macros = "!include " + mainpath + "pumla_macros_global.puml\n"
+pm_include_tv = "!include " + mainpath + "pumla_tagged_values.puml\n"
+with open(pumla_macros_fn, "w") as fil:
+    fil.write(pm_comment)
+    fil.write(pm_include_macros)
+    fil.write(pm_include_tv)
+fil.close()
