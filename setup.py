e0bf434e3167af4de5a21011a441ad8e8dd8df50
Dr. Markus Voss <voss.markus@gmx.net>
2021-09-22 09:57:54 +0200

    post install scripts are not supported and intended with wheels... so need to include in install procedure another manual step

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 """setup procedure"""
 from setuptools import setup, find_packages
-import os
 
 setup(
     packages=find_packages(where="src"),
@@ -8,19 +7,3 @@ setup(
     extras_require={"dev": ["mypy==0.910", "pylint==2.10.2"]},
 )
 
-curpath = os.path.abspath(os.path.curdir)
-print("Path to initialise: " + curpath)
-print("Path of pumla installation: " + curpath)
-mainpath = curpath + "/"
-pumla_macros_fn = curpath + "/test/examples/pumla_macros.puml"
-pm_comment = "' THIS IS AN AUTOMATICALLY GENERATED FILE BY pumla init \n"
-pm_comment = pm_comment + "' DO NOT CHANGE MANUALLY!\n"
-pm_comment = pm_comment + "' TO ADOPT THE PATHS TO YOUR SYSTEM, CALL pumla init AGAIN\n"
-pm_comment = pm_comment + "' IN THE FOLDER OF THIS FILE HERE!\n"
-pm_include_macros = "!include " + mainpath + "pumla_macros_global.puml\n"
-pm_include_tv = "!include " + mainpath + "pumla_tagged_values.puml\n"
-with open(pumla_macros_fn, "w") as fil:
-    fil.write(pm_comment)
-    fil.write(pm_include_macros)
-    fil.write(pm_include_tv)
-fil.close()
