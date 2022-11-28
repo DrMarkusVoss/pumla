67080bf05ec887b7e208c6cd8f359be6e01482c7
Dr. Markus Voss <voss.markus@gmx.net>
2022-11-28 08:17:21 +0100

    correct setup.py (putting the requests package here as install dependency creates problems with the automatic tests, have to check out whats wrong later).

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires={["requests"]},
     extras_require={"dev": ["mypy==0.910", "pylint==2.10.2"]},
 )
 
